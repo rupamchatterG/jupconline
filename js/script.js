d924a0e1747694c6df2e5fca9f84145b8a442895
JUPC <138682808+jupconline@users.noreply.github.com>
2024-01-06 18:13:42 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -90,4 +90,16 @@ function showSlides() {
 
 //  showSlidesCurrent(slideIndexCurrent);
 
+window.addEventListener("load", function(){
+  setTimeout(
+      function open(event){
+          document.querySelector(".popup").style.display = "block";
+      },
+      1000
+  )
+});
+document.querySelector("#close").addEventListener("click", function(){
+  document.querySelector(".popup").style.display = "none";
+});
+
 
